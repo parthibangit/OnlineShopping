e3f8f18de932e54251d920439b320db115fc2cc4
parthibangit <sarathipartha04@gmail.com>
2026-07-26 15:43:24 +0530

    issues fixed to publish html report.

--- playwright.config.js
+++ playwright.config.js
@@ -34,8 +34,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['list'], 
-    ['html', {open: 'never'}]
+    ['list'],
+    ['html', { open: 'never', outputFolder: 'playwright-report' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
